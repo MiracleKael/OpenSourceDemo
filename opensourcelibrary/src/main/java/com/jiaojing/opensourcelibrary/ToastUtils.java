8e66f8b15a9785586554e555b2d25ef2e9599941
jiaojing <jiaojingldy@163.com>
2017-12-11 11:33:51 +0800

    Initial commit

---
+++ opensourcelibrary/src/main/java/com/jiaojing/opensourcelibrary/ToastUtils.java
@@ -0,0 +1,20 @@
+package com.jiaojing.opensourcelibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by jiaojing on 2017/12/11.
+ */
+
+public class ToastUtils {
+    /**
+     * 显示吐司
+     *
+     * @param context
+     * @param str
+     */
+    public static void showToast(Context context, String str) {
+        Toast.makeText(context, str, Toast.LENGTH_LONG).show();
+    }
+    }
